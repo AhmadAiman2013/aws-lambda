5acd42d3daf4f38246c9a80db732d67930f6122f
ahmad aiman <ahmadaimanalias@gmail.com>
2026-02-18 18:17:32 +0800

    feat: add cors config

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::env::set_var;
+use axum::http::header::{AUTHORIZATION, CONTENT_TYPE};
+use axum::http::Method;
 use axum::Router;
 use axum::routing::{get, post};
 use lambda_http::{run, tracing, Error};
+use tower_http::cors::CorsLayer;
+
 mod axum_handler;
 
 use crate::axum_handler::{get_github_sucess, get_name, get_parameters, health_check};
@@ -12,12 +16,20 @@ async fn main() -> Result<(), Error> {
 
     tracing::init_default_subscriber();
 
+    let origins = ["http://localhost:4200".parse()?];
+
+    let cors = CorsLayer::new()
+        .allow_origin(origins)
+        .allow_methods([Method::GET, Method::POST, Method::PUT, Method::DELETE, Method::OPTIONS])
+        .allow_headers([AUTHORIZATION, CONTENT_TYPE]);
+
     let app = Router::new()
         .route("/", get(health_check))
         .route("/health", get(health_check))
         .route("/params", post(get_parameters))
         .route("/github", get(get_github_sucess))
-        .route("/hello/{name}", get(get_name));
+        .route("/hello/{name}", get(get_name))
+        .layer(cors);
 
 
     run(app).await
